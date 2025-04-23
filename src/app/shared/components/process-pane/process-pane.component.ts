afaccb747ec24c883e3ab216b0fc09fb6d50fedf
Tagor Petrosian <tagorpe@matrix.com>
2025-04-23 13:04:34 +0300

    mode processpane

--- src/app/shared/components/process-pane/process-pane.component.ts
+++ src/app/shared/components/process-pane/process-pane.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-process-pane',
   templateUrl: './process-pane.component.html',
   styleUrls: ['./process-pane.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 })
 export class ProcessPaneComponent {
-  @Input() headerTitle: string = '';
-  @Input() buttonText: string = 'המשך בבקשה';
+  @Input() headerTitle: string = 'בקשה להרחבת מסד נתונים';
+  @Input() buttonText: string = 'עזרה ומידע נוסף';
   @Output() buttonClick = new EventEmitter<void>();
 
   onButtonClick(): void {
