dd2822993e1179b98529610c17a14f82e05fa6ed
Tagor Petrosian <tagorpe@matrix.com>
2025-04-23 12:52:08 +0300

    add shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    // Shared components, directives, and pipes will go here
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    // Re-export modules that should be available to importing modules
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    // Also export all declarations so they're available to any module that imports SharedModule
+  ]
+})
+export class SharedModule { }
